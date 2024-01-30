f8a89235ae73be1c7279ed22cf1a5653ee4b6a24
iphonesiri1996 <navjyotabhale0105@gmail.com>
2024-01-30 10:29:34 +0530

    html changes

--- settings.py
+++ settings.py
@@ -145,4 +145,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False
-}
+    STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
